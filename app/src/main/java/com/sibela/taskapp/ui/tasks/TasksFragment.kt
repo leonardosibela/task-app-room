9e179ea1f31f1dd25ad7cacbe5b48d2fd57cfee1
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-13 20:50:07 -0300

    Implementing TasksFragment and TasksViewModel

--- app/src/main/java/com/sibela/taskapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/sibela/taskapp/ui/tasks/TasksFragment.kt
@@ -1,8 +1,12 @@
 package com.sibela.taskapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sibela.taskapp.R
+import com.sibela.taskapp.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,4 +14,21 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+        val taskAdapter = TasksAdapter()
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) { tasks ->
+            taskAdapter.submitList(tasks)
+        }
+    }
 }
--- app/src/main/java/com/sibela/taskapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/sibela/taskapp/ui/tasks/TasksViewModel.kt
@@ -2,12 +2,13 @@ package com.sibela.taskapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.sibela.taskapp.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
 
-
+    val tasks = taskDao.getTasks().asLiveData()
 
 }
